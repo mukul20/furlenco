e6955f447586e1a00f8ba4d071629fc7e7cf967e
Mukul Singh <mukulsingh@Mukuls-MacBook-Pro.local>
2023-01-22 11:17:29 +0530

    update

--- resources/views/components/email/footer.blade.php
+++ resources/views/components/email/footer.blade.php
@@ -1,10 +1,24 @@
 <div style="padding:3rem 4rem;border-top:1px solid #e9e9e9;line-height:0;">
-	<img style="margin-right:1.5rem" src="{{$assetPath}}/images/fb-icon.png" width="32" height="32" />
-	<img style="margin-right:1.5rem" src="{{$assetPath}}/images/fb-icon.png" width="32" height="32" />
-	<img src="{{$assetPath}}/images/fb-icon.png" width="32" height="32" />
+	<a href="https://www.facebook.com/Furlenco" style="text-decoration:none;">
+		<img style="margin-right:1.4rem" src="{{$assetPath}}/images/fb-icon.png" width="32" height="32" />
+	</a>
+	<a href="https://www.instagram.com/furlenco" style="text-decoration:none;">
+		<img style="margin-right:1.4rem" src="{{$assetPath}}/images/insta-icon.png" width="32" height="32" />
+	</a>
+	<a href="https://www.twitter.com/furlenco" style="text-decoration:none;">
+		<img src="{{$assetPath}}/images/twitter-icon.png" width="32" height="32" />
+	</a>
 	<ul class="float-end" style="list-style:none;margin-top:0;">
-		<li style="display:inline-block;"><img src="{{$assetPath}}/images/apple-icon.png" width="24" /></li>
+		<li style="display:inline-block;">
+			<a href="https://apps.apple.com/app/furlenco/id1134919232" style="text-decoration:none;">
+				<img src="{{$assetPath}}/images/apple-icon.png" width="24" />
+			</a>
+		</li>
 		<li style="display:inline-block;border:1px solid #222222;margin:0 0.5rem;height:25px;"></li>
-		<li style="display:inline-block;"><img src="{{$assetPath}}/images/apple-icon.png" width="24" /></li>
+		<li style="display:inline-block;">
+			<a href="https://play.google.com/store/apps/details?id=com.furlenco.android" style="text-decoration:none;">
+				<img src="{{$assetPath}}/images/playstore-icon.png" width="24" />
+			</a>
+		</li>
 	</ul>
 </div>
