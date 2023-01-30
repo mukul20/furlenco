68d2f68a583fb66444d414564f8e4b40ff6d705b
Mukul Singh <mukulsingh@Mukuls-MacBook-Pro.local>
2023-01-31 00:03:39 +0530

    update

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,140 +1,74 @@
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </head>
-    <div style="background: #f0f0ef;">
-        <div style="background: #fff; width: 640px; margin: 0 auto; font-size: 18px; font-family: sans-serif; color: rgb(34, 34, 34); line-height: 1.4; border: solid #059baa; border-width: 0 1px;">
-            <div style="padding: 3.5rem 4rem 3rem 4rem; background: #059baa; color: #fff;">
-                <img src="https://raw.githubusercontent.com/mukul20/furlenco/master/public/images/logo.png" style="width: 250px; max-width: 100%;" />
-                <a href="https://www.furlenco.com/" style="text-decoration: none; float: right; color: inherit;">www.furlenco.com</a>
-            </div>
-            <div style="padding: 0 2rem 3rem 2rem; background: #059baa; color: #fff;">
-                <div style="border-radius: 1.4rem; padding: 2.5rem; background: #fff; color: rgb(34, 34, 34);">
-                    <p style="margin: 0; font-weight: bold; font-size: 24px; color: #059baa;">ACTIVITY DELAYED</p>
-                    <p style="margin-bottom: 15px; font-size: 26px;">Hi Sauban Ahmad,</p>
-                    <p style="margin: 0;">
-                        We regret to inform you that your Furlenco request has been delayed.<br />
-                        <br />
-                        We know this is frustrating and we’re trying our best to complete your request as soon as possible.
-                    </p>
-                </div>
-            </div>
-            <div style="padding: 2.5rem;">
-                <p style="margin: 0; font-weight: bold; font-size: 24px; color: #059baa;">Activity details:</p>
-                <table style="width: 100%; border-spacing: 0 1.8rem;">
-                    <tr style="font-size: 16px;">
-                        <th colspan="2" style="text-align: left; padding-left: 0; width: 60%; padding: 0.5rem 0.5rem 0.5rem 0;">Product Details</th>
-                        <th style="padding: 0.5rem;">Qty</th>
-                        <th style="padding: 0;">Activity</th>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div
-                                style="
-                                    background: #fff;
-                                    width: 100px;
-                                    height: 100px;
-                                    text-align: center;
-                                    display: flex;
-                                    justify-content: center;
-                                    border-radius: 1rem;
-                                    -webkit-box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-                                    -moz-box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-                                    box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-                                "
-                            >
-                                <img src="https://raw.githubusercontent.com/mukul20/furlenco/master/public/images/sofa-icon.png" style="max-width: 100%; margin: auto; display: block; max-height: 100%;" />
-                            </div>
-                        </td>
-                        <td style="padding: 1.5rem; padding-right: 0;">Vive Lilac Three Seater Couch</td>
-                        <td style="padding: 1.5rem; text-align: center;">x1</td>
-                        <td style="padding: 0; text-align: center;">DELIVERY</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div
-                                style="
-                                    background: #fff;
-                                    width: 100px;
-                                    height: 100px;
-                                    text-align: center;
-                                    display: flex;
-                                    justify-content: center;
-                                    border-radius: 1rem;
-                                    -webkit-box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-                                    -moz-box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-                                    box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-                                "
-                            >
-                                <img src="https://raw.githubusercontent.com/mukul20/furlenco/master/public/images/sofa-icon.png" style="max-width: 100%; margin: auto; display: block; max-height: 100%;" />
-                            </div>
-                        </td>
-                        <td style="padding: 1.5rem; padding-right: 0;">Vive Lilac Three Seater Couch</td>
-                        <td style="padding: 1.5rem; text-align: center;">x1</td>
-                        <td style="padding: 0; text-align: center;">DELIVERY</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div
-                                style="
-                                    background: #fff;
-                                    width: 100px;
-                                    height: 100px;
-                                    text-align: center;
-                                    display: flex;
-                                    justify-content: center;
-                                    border-radius: 1rem;
-                                    -webkit-box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-                                    -moz-box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-                                    box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.1);
-                                "
-                            >
-                                <img src="https://raw.githubusercontent.com/mukul20/furlenco/master/public/images/sofa-icon.png" style="max-width: 100%; margin: auto; display: block; max-height: 100%;" />
-                            </div>
-                        </td>
-                        <td style="padding: 1.5rem; padding-right: 0;">Vive Lilac Three Seater Couch</td>
-                        <td style="padding: 1.5rem; text-align: center;">x1</td>
-                        <td style="padding: 0; text-align: center;">DELIVERY</td>
-                    </tr>
-                </table>
-                <hr style="border-top: 1px solid #bcbcbc;" />
-                <br />
-                You can always check the <a href="#" style="font-weight: bold; color: inherit;">Furlenco app</a> to stay updated on your Furlenco activity. <br />
-                <br />
-                For further queries or issues, do take a look at our <a href="#" style="font-weight: bold; color: inherit;">FAQ section.</a> <br />
-                <br />
-                Lots of love and furniture, <br />
-                <br />
-                Team Furlenco.
-            </div>
-            <div style="padding: 2.8rem 4rem; border-top: 1px solid #e9e9e9; line-height: 0;">
-                <a href="https://www.facebook.com/Furlenco" style="text-decoration: none; color: inherit;">
-                    <img style="margin-right: 1.4rem; max-width: 100%;" src="https://raw.githubusercontent.com/mukul20/furlenco/master/public/images/fb-icon.png" width="32" height="32" />
-                </a>
-                <a href="https://www.instagram.com/furlenco" style="text-decoration: none; color: inherit;">
-                    <img style="margin-right: 1.4rem; max-width: 100%;" src="https://raw.githubusercontent.com/mukul20/furlenco/master/public/images/insta-icon.png" width="32" height="32" />
-                </a>
-                <a href="https://www.twitter.com/furlenco" style="text-decoration: none; color: inherit;">
-                    <img src="https://raw.githubusercontent.com/mukul20/furlenco/master/public/images/twitter-icon.png" width="32" height="32" style="max-width: 100%;" />
-                </a>
-                <ul style="list-style: none; margin-top: 0; padding-left: 0; float: right;">
-                    <li style="display: inline-block;">
-                        <a href="https://apps.apple.com/app/furlenco/id1134919232" style="text-decoration: none; color: inherit;">
-                            <img src="https://raw.githubusercontent.com/mukul20/furlenco/master/public/images/apple-icon.png" width="24" style="max-width: 100%;" />
-                        </a>
-                    </li>
-                    <li style="display: inline-block; border: 1px solid #222222; margin: 0 0.5rem; height: 25px;"></li>
-                    <li style="display: inline-block;">
-                        <a href="https://play.google.com/store/apps/details?id=com.furlenco.android" style="text-decoration: none; color: inherit;">
-                            <img src="https://raw.githubusercontent.com/mukul20/furlenco/master/public/images/playstore-icon.png" width="24" style="max-width: 100%;" />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <hr style="width: 640px; margin: 0; border-width: 1px 0 0 0; border-top: 1px solid #059baa;" />
+@extends('layouts.email')
+@section('content')
+<div class="fl-container">
+    @include('components.email.header')
+    <div class="pad-standard">
+        <span class="fl-color bold ft-24">WELCOME TO FURLENCO</span>
+        <p class="ft-26 mb-10">Hey Sauban Ahmad,</p>
+        It is time to live free.
+        <br/><br/>
+        <img class="float-end" src="{{$assetPath}}/images/banner.png" width="90%" />
+        <div class="clear"></div>
+        <br/>
+        <div class="text-center">
+            <span class="fl-color ft-40 in-bl" style="margin-bottom:5px;">Introducing true freedom</span><br/>
+            <span class="ft-24">to the world of furniture</span>
         </div>
     </div>
-</html>
+    <div class="text-center" style="margin:0 2rem 2rem 2rem;padding:2rem;background:#e6f5f8;border-radius:1.5rem;">
+        <p class="m-0">You know what's best for you. At Furlenco, we give you the freedom to bring furniture to your home on your terms. With world-class designs & unparalleled quality, we give you the power to create a home that’s always trendy and always yours.</p>
+    </div>
+    <div class="row">
+        <div class="col-7 text-center">
+            <p class="ft-24 mb-0">The Freedom to</p>
+            <span class="fl-color ft-56">CHOOSE</span>
+            <p class="mt-5 p-y2-x4" style="background:#ffeffa;border-radius:0 1.5rem 1.5rem 0;">
+                Choose how you want your furniture - rent short term, buy brand new or buy refurbished.
+            </p>
+        </div>
+        <div class="col-5" style="padding-top:5rem">
+            <img src="{{$assetPath}}/images/chair1.png" />
+        </div>
+    </div>
+    <br/><br/>
+    <div class="row">
+        <div class="col-5" style="padding-top:7rem;">
+            <img class="float-end" src="{{$assetPath}}/images/chair2.png" width="90%" />
+        </div>
+        <div class="col-7 text-center">
+            <p class="ft-24 m-0">The Freedom to</p>
+            <span class="fl-color ft-56">CHANGE</span>
+            <p class="mt-5 p-y2-x4" style="background:#ffeffa;border-radius:1.5rem 0 0 1.5rem;">
+                Change as your needs change. Own the furniture you once rented, or sell back your old products for credits you can use to buy something new.
+            </p>
+        </div>
+    </div>
+    <center>
+        <span style="background:#fffced;border-radius:1rem;padding:0.8rem 1.2rem;margin:2rem 0;display:inline-block;">
+            Here's our <a class="bold" href="#">listing</a> on our latest products and collections.
+        </span>
+    </center>
+    <br/>
+    <div class="fl-bg pad-standard text-center">
+        <span class="ft-40 fl-yellow">
+            0% compromise.<br/>
+            100% excellence.
+        </span>
+        <br/>
+        @for ($x = 0; $x < 3; $x++)
+        <div class="col-9" style="margin:0 auto">
+            <center>
+                <img style="margin-top:2rem;" src="{{$assetPath}}/images/smiley.png" width="120" />
+            </center>
+            <p class="ft-24 bold">World-class designer furniture</p>
+            <p>Our inhouse designers create the best of aesthetics, form & functionality to create truely gorgeous furniture.</p>
+        </div>
+        @endfor
+    </div>
+    <div class="pad-standard">
+        <p>Something on your mind? Check out our <a class="bold" href="#">FAQ section</a> for queries and details.</p>
+        <p>We hope you have fun with Furlenco!</p>
+    </div>
+    @include('components.email.footer')
+</div>
+@endsection
